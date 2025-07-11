64e34c2076c14a67929a3e32f324131df706a837
Nicolas BEGUIER <nicolas_beguierwhotmail.com>
2025-07-11 22:11:00 +0200

    Add octogons and 1972_JEB_OJO.txt

--- decipher.py
+++ decipher.py
@@ -20,8 +20,8 @@ def main():
 
     args = parser.parse_args()
     encrypted_input = "01 234560 7898a23bc def049e dec 0g 32d4fc dec e4h6i6dj 07ck djle03 dec 78bd0kj de6390 21dck dec m61c n1"
-    encrypted_input = "12 3456 7859 ab 6 4b5cd b6"
-    encrypted_input = "74d9b6 ad4 19b6 edbf16b6 b9 8 g8h816 13 f4hed16b6"
+    encrypted_input = "dec m61c n1 de6390 21dck dec 78bd0kj 07ck djle03 dec e4h6i6dj 0g 32d4fc def049e dec 7898a23bc 01 234560"
+    encrypted_input = "01 2345 6748 9a5 3a4bca5 63cda5 9c30da5 ecaf05a5 ad 7 g7h705 02f3hec05a5"
 
     decrypted_output = decrypt(args.mapping, encrypted_input)
     print(decrypted_output)
